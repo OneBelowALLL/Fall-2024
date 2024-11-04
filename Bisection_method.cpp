7c628e794b4236d1c0126a65ec1c210d0c144b90
Md. Hasib Hossain <127829562+OneBelowALLL@users.noreply.github.com>
2024-11-05 00:18:31 +0600

    Add files via upload
    this a code for the Bisection_Method to find the root of an Equation

---
+++ Bisection_method.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define FlashIO()       (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+const double Max_diff = 1e-5;
+
+auto Equation = [](double x) { return x * x * x - 4 * x - 9; };
+
+double Bisection_Method(double a,double b)
+{
+	//the product of f(a) and f(b) have to be negative otherwise it won't work
+	if(Equation(a) * Equation(b) >= 0)
+	{
+		cout << "Given interval is not valid" << '\n';
+		return 0;
+	}
+
+	double c = a;
+	while((b - a) >= Max_diff)
+	{
+		//finding the midpoint
+		c = (a + b) / 2;
+
+		//checking if we got our desired root if we do we will break the loop and return or root
+		if(fabs(Equation(c)) < Max_diff or (b - a) < Max_diff)
+		{
+			break;
+		}
+
+		//switching our interval
+		if(Equation(c) * Equation(a) < 0)
+		{
+			b = c;
+		}
+		else 
+		{
+			a = c;
+		}
+	}
+
+	//returning our final root to main function
+	return c;
+}
+
+int32_t main()
+{
+	FlashIO();
+
+	//taking input of the interval
+	double a,b;
+	cout << "Enter the interval [a,b] : " ;
+	cin >> a >> b;
+
+	//calling the function to calculate the root
+	double root = Bisection_Method(a,b);
+	
+	//if root is not zero then we have a valid answer
+	if(root != 0.00)
+	{
+		cout << "The root is : " << root << '\n';
+	}
+
+	return 0;
+}
